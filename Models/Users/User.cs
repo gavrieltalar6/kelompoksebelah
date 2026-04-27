5371363e5ea5c890398231a16564f044f583538c
gibekss23 <gibekss23@gmail.com>
2026-04-27 13:18:38 +0700

    xaml kasir

--- Models/Users/User.cs
+++ Models/Users/User.cs
@@ -2,7 +2,7 @@ using CakeProject.Models; // Baris ini memberi tahu VS Code untuk melihat folder
 
 namespace CakeProject.Models.Users; // Tambahkan .Users
 
-public abstract class User : BaseEntity 
+public abstract class User : BaseEntity
 {
     public string Nama { get; set; }
     public string NoHP { get; set; }
